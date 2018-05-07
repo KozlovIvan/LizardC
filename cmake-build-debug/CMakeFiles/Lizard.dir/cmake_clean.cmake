7e6329b22e5393c6fe5be6d9df7a46c097848fac
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-07 16:28:57 +0200

    [master] : initial commit

---
+++ cmake-build-debug/CMakeFiles/Lizard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lizard.dir/main.c.o"
+  "Lizard.pdb"
+  "Lizard"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lizard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
