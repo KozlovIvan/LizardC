38c9247aef70511e83604d156773052c663083cc
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-08 00:18:33 +0200

    [master] : initialized skeleton of the project with all needed components

--- main.c
+++ main.c
@@ -1,6 +1,67 @@
 #include <stdio.h>
 
+void _construct(/*key, IV, length*/){
+    //TODO
+}
+
+void _initialization(/* key, IV */){
+    //TODO
+}
+
+void loadkey(/*key*/){
+    //TODO
+}
+
+void loadIV(/*IV*/){
+    //TODO
+}
+
+void initRegisters(){
+    //TODO
+}
+
+void mixing(){
+    //TODO
+}
+
+void a(){
+    //TODO
+}
+
+void NFSR1(){
+    //TODO
+}
+
+void keyadd(){
+    //TODO
+}
+
+void diffusion(){
+    //TODO
+}
+
+void keysteamGeneration(/*length*/){
+    //TODO
+}
+
+void keystreamGenerationSpecification(/*length*/){
+    //TODO
+}
+
+void getKeystream(){
+    //TODO
+}
+
+void binArray2hex(/*bin*/){
+    //TODO
+}
+
+void hex2binArray(/*hex*/){
+    //TODO
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    //TODO
     return 0;
 }
