d5b093b79b884eb1c236c262ae6be8b9b6f316ae
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-11 18:48:30 +0200

    [master]: added loading of key and initial vector

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/*
+ * Input formats must be 120 bit key as 30 hex values and 64 bit IV as 16 hex values.
+ * The keystream output length can be chosen between 1 bit and 10000 bits.
+ */
+//key
+u_int8_t K[30];
+//iv
+u_int8_t IV[16];
+// bool a257 = false;
+u_int8_t a257 = 0;
+
+
+
+
 void _construct(/*key, IV, length*/){
     //TODO
 }
@@ -8,12 +22,17 @@ void _initialization(/* key, IV */){
     //TODO
 }
 
-void loadkey(/*key*/){
-    //TODO
+void loadkey(u_int8_t *key){
+
+    for (int i = 0; i < 119; ++i){
+        K[i] = key[i];
+    }
 }
 
-void loadIV(/*IV*/){
-    //TODO
+void loadIV(u_int8_t *iv){
+    for (int i = 0; i < 63; ++i){
+        IV[i] = iv[i];
+    }
 }
 
 void initRegisters(){
