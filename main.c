04ed4f8ea50d5c8ec88fdb6731cdc9b9dbd149bc
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-12 13:51:50 +0200

    [master]: register initiliazation completed

--- main.c
+++ main.c
@@ -4,10 +4,16 @@
  * Input formats must be 120 bit key as 30 hex values and 64 bit IV as 16 hex values.
  * The keystream output length can be chosen between 1 bit and 10000 bits.
  */
-//key
-u_int8_t K[30];
-//iv
-u_int8_t IV[16];
+//key bin
+u_int8_t K[120];
+//iv bin
+u_int8_t IV[64];
+
+//B
+u_int8_t B[258][90];
+//S
+u_int8_t S[258][31];
+
 // bool a257 = false;
 u_int8_t a257 = 0;
 
@@ -36,7 +42,21 @@ void loadIV(u_int8_t *iv){
 }
 
 void initRegisters(){
-    //TODO
+    
+    for(int i = 0; i <=63; ++i){
+       B[0][i] = K[i]^IV[i];
+    }
+
+    for(int i = 64; i<=89; ++i){
+        B[0][i] = K[i];
+    }
+
+    for(int i = 0; i <=28; ++i){
+       S[0][i] = K[i+90];
+    }
+
+   S[0][29] = K[119] ^ 1;
+   S[0][30] = 1;
 }
 
 void mixing(){
