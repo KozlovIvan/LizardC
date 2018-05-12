abc5a63dcb65e1aaa1eb3adf4f84fe6fb18914c0
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-12 23:24:59 +0200

    [master] : header file up to date

--- lizard.h
+++ lizard.h
@@ -15,6 +15,9 @@ void keyadd();
 void diffusion();
 u_int8_t NFSR1();
 u_int8_t NFSR2();
+void _construct(u_int8_t  *key, u_int8_t *iv, int length);
+void _initialization(u_int8_t *key, u_int8_t *iv);
+void keysteamGeneration(int length);
 
 
 #endif //LIZARD_LIZARD_H
